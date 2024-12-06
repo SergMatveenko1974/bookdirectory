7c7c0b5b0a9ed9b97114a925555e3b4a5daf8e2b
Serg1974 <matveich@tut.by>
2024-12-06 13:27:24 +0100

    frontend initial setup

---
+++ frontend/app/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p class="reed-the-docs">This is a full stack  project build with the help of wiee.js and node.js</p>
+  
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
+
